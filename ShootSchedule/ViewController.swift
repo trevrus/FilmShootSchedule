74c5142077d284e6410f9b98bc6af2315d8bf597
Trevor Smith <4trevor@gmail.com>
2018-04-10 07:43:26 -0700

    Initial Commit

---
+++ ShootSchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShootSchedule
+//
+//  Created by Trevor Smith on 2018-04-10.
+//  Copyright © 2018 Westleaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
